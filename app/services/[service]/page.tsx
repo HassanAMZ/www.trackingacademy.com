ee57aa631edf95da00e78af794c9544a8321b223
Shahzada Ali Hassan <reactjswebdev@gmail.com>
2023-06-02 14:16:46 +0800

    Fixed Service Page

--- app/services/[service]/page.tsx
+++ app/services/[service]/page.tsx
@@ -1,6 +1,6 @@
 import servicesDetails from "@/data/services-details";
 import Image from "next/image";
-
+import Link from "next/link";
 type ServiceDetails = {
  pid: string;
  title: string;
@@ -27,39 +27,76 @@ const Page: React.FC<PageProps> = ({ params }) => {
  );
 
  if (serviceObject) {
+  let serviceSubDetails = serviceObject.packages.map((service) => {
+   return (
+    <>
+     <p className='col-span-3'>{service.name}</p>
+     <div className='col-span-2 grid grid-cols-1 gap-2'>
+      {service.value.slice(0, 1).map((value) => {
+       return <p>{value}</p>;
+      })}
+     </div>
+    </>
+   );
+  });
+
+  let buyButton = (
+   <div className='px-4 pb-4 flex flex-col'>
+    <Link
+     href='#'
+     className='rounded-md bg-gradient-to-tr from-my-purple to-my-pink px-3.5 py-2.5 text-center font-semibold shadow-sm hover:to-my-purple hover:from-my-pink focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-my-purple'>
+     Continue ( {serviceObject.packages[1].value[0]}$ )
+    </Link>
+   </div>
+  );
   return (
    <>
     <h1 className='text-4xl font-semibold'>{serviceObject.title}</h1>
-    <div className=''>
-     <div className=''>
-      <div className='relative aspect-[4/3] sm:h-auto max-w-[150px] sm:max-w-full'>
+    <div className='grid grid-cols-1 md:grid-cols-[3fr,_2fr]'>
+     <div className='gap-2 flex flex-col'>
+      <div className='relative aspect-[4/3]'>
        <Image
         className='rounded-lg'
-        width={1000}
-        height={750}
+        fill
         src={serviceObject.featured_image_url}
         alt={serviceObject.title}
        />
       </div>
+
+      <div className='flex flex-col md:hidden border-2 border-gray-50 rounded-md'>
+       <div className='grid grid-cols-5 gap-2 p-4'>{serviceSubDetails}</div>
+       {buyButton}
+      </div>
+
       <div>
-       <h3 className='text-xl'> Project Details</h3>
+       <h3 className='text-2xl font-semibold py-2'> Project Details</h3>
        <p
         dangerouslySetInnerHTML={{
          __html: serviceObject.description.replace(/\n/g, "<br>"),
         }}
        />
       </div>
-      <div className='flex flex-col'>
-       <div className='flex gap-3 opacity-80'>
-        <p className='bg-gray-800 bg-opacity-[30%] rounded-lg max-w-fit py-2'>
-         from $ {serviceObject.packages[1].value[0]}
-        </p>
-        <p className='py-2'>{serviceObject.packages[2].value[2]} &nbsp;</p>
-       </div>
-       d
+      <div className='grid grid-cols-5 gap-2 p-4 border-2 border-gray-50 rounded-md'>
+       {serviceObject.packages.map((service) => {
+        return (
+         <>
+          <p className='col-span-2'>{service.name}</p>
+          <div className='col-span-3 grid grid-cols-3 gap-2'>
+           {service.value.map((value) => {
+            return <p>{value}</p>;
+           })}
+          </div>
+         </>
+        );
+       })}
       </div>
      </div>
-     <div className=''></div>
+     <div className='hidden md:block px-2'>
+      <div className='flex flex-col border-2 border-gray-50 rounded-md'>
+       <div className='grid grid-cols-5 gap-2 p-4'>{serviceSubDetails}</div>
+       {buyButton}
+      </div>
+     </div>
     </div>
    </>
   );
