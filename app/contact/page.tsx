3ea420c4abae3ed118e406ed48bd41d4175505e5
Shahzada Ali Hassan <reactjswebdev@gmail.com>
2023-10-04 01:42:23 +0800

    Fixed height of the calendly event page.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,23 +1,41 @@
-// Page component
 "use client";
 
-import React from "react";
-import { InlineWidget } from "react-calendly";
-import { GTMCalendlyEvent } from "@/components/analytics/GTMEvents"; // Update the import path as per your directory structure
+import React, { useState } from "react";
+import { InlineWidget, useCalendlyEventListener } from "react-calendly";
+import { GTMCalendlyEvent } from "@/components/analytics/GTMEvents";
 import CustomHeader from "@/components/home/CustomHeader";
 
 const Page = () => {
+ // Define a state variable to keep track of the height
+ const [height, setHeight] = useState("100vh");
+
+ // Set up event listeners using the useCalendlyEventListener hook
+ useCalendlyEventListener({
+  onProfilePageViewed: () => {
+   setHeight("110vh");
+  },
+  onEventTypeViewed: () => {
+   setHeight("120vh");
+  },
+  onDateAndTimeSelected: () => {
+   setHeight("170vh");
+  },
+  onEventScheduled: () => {
+   setHeight("100vh");
+  },
+ });
+
  return (
-  <>
+  <React.Fragment>
    <GTMCalendlyEvent />
    <CustomHeader text='Schedule a meeting' />
    <div className='backgroundOverlay'>
     <InlineWidget
      url='https://calendly.com/shahzadaalihassan'
-     styles={{ height: "130vh" }}
+     styles={{ height: height }} // Use the height state variable here
     />
    </div>
-  </>
+  </React.Fragment>
  );
 };
 
