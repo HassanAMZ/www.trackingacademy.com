95b4f0f32cfe0ff0df394f676d08a773d6134164
Shahzada Ali Hassan <reactjswebdev@gmail.com>
2024-01-05 00:05:56 +0800

    Added Filters in Blog

--- components/blog/BlogSearch.tsx
+++ components/blog/BlogSearch.tsx
@@ -8,32 +8,79 @@ import {
  Paragraphlg,
  Paragraphsm,
 } from "@/components/typography/Heading";
-import ContainerLayout from "../layouts/ContainerLayout";
+
+const cmsOptions = ["Shopify", "WordPress", "Joomla", "React", "Others"];
+const pixelOptions = ["GA4", "Facebook Pixel", "TikTok Pixel"];
+const eventOptions = [
+ "Page View",
+ "View Item",
+ "Begin Checkout",
+ "Add To Cart",
+ "Purchase",
+];
+const integrationOptions = ["Plugin", "Google Tag Manager", "Gtag"];
 
 const BlogSearch: React.FC<BlogSearchProps> = ({ data, onSearch }) => {
  const [searchTerm, setSearchTerm] = useState("");
- const [results, setResults] = useState<PostMetadata[]>(data); // initialize with all data
-
+ const [results, setResults] = useState<PostMetadata[]>(data);
+ const [selectedCMS, setSelectedCMS] = useState("");
+ const [selectedPixel, setSelectedPixel] = useState("");
+ const [selectedEvent, setSelectedEvent] = useState("");
+ const [selectedIntegration, setSelectedIntegration] = useState("");
+ console.log(data[0]);
  useEffect(() => {
   const searchWords = searchTerm
    .toLowerCase()
    .split(" ")
    .filter((word) => word);
-
   const filtered = data.filter((post) => {
-   return searchWords.every((word) => {
-    // Check if word is in title, description, or tags without creating a RegExp object
-    return (
+   const matchesSearchTerm = searchWords.every(
+    (word) =>
      post.title.toLowerCase().includes(word) ||
      post.description.toLowerCase().includes(word) ||
      post.tags.some((tag) => tag.toLowerCase().includes(word))
-    );
-   });
+   );
+
+   const matchesDropdowns =
+    (selectedCMS === "" ||
+     post.title.toLowerCase().includes(selectedCMS.toLowerCase()) ||
+     post.description.toLowerCase().includes(selectedCMS.toLowerCase()) ||
+     post.tags.some((tag) =>
+      tag.toLowerCase().includes(selectedCMS.toLowerCase())
+     )) &&
+    (selectedPixel === "" ||
+     post.title.toLowerCase().includes(selectedPixel.toLowerCase()) ||
+     post.description.toLowerCase().includes(selectedPixel.toLowerCase()) ||
+     post.tags.some((tag) =>
+      tag.toLowerCase().includes(selectedPixel.toLowerCase())
+     )) &&
+    (selectedEvent === "" ||
+     post.title.toLowerCase().includes(selectedEvent.toLowerCase()) ||
+     post.description.toLowerCase().includes(selectedEvent.toLowerCase()) ||
+     post.tags.some((tag) =>
+      tag.toLowerCase().includes(selectedEvent.toLowerCase())
+     )) &&
+    (selectedIntegration === "" ||
+     post.title.toLowerCase().includes(selectedIntegration.toLowerCase()) ||
+     post.description
+      .toLowerCase()
+      .includes(selectedIntegration.toLowerCase()) ||
+     post.tags.some((tag) =>
+      tag.toLowerCase().includes(selectedIntegration.toLowerCase())
+     ));
+
+   return matchesSearchTerm && matchesDropdowns;
   });
 
   setResults(filtered);
   onSearch(filtered);
- }, [searchTerm]);
+ }, [
+  searchTerm,
+  selectedCMS,
+  selectedPixel,
+  selectedEvent,
+  selectedIntegration,
+ ]);
 
  return (
   <div className='flex flex-col pt-6 pb-1 sm:py-12 items-left sm:items-center sm:text-center justify-center w-full gap-4'>
@@ -67,6 +114,61 @@ const BlogSearch: React.FC<BlogSearchProps> = ({ data, onSearch }) => {
      onChange={(e) => setSearchTerm(e.target.value)}
      className='p-1 rounded-full text-gray-800 dark:text-white bg-transparent peer h-full w-full outline-none text-sm  pr-2'
     />
+   </div>
+
+   {/* Dropdowns */}
+   <div className='flex flex-wrap sm:justify-center gap-3 '>
+    {/* CMS Selector */}
+    <select
+     value={selectedCMS}
+     onChange={(e) => setSelectedCMS(e.target.value)}
+     className={`py-2 px-4 rounded-full inline-flex items-center bg-white border  cursor-pointer  text-gray-700 dark:bg-gray-800 dark:text-white`}>
+     <option value=''>Select CMS</option>
+     {cmsOptions.map((cms) => (
+      <option key={cms} value={cms}>
+       {cms}
+      </option>
+     ))}
+    </select>
+
+    {/* Pixel Selector */}
+    <select
+     value={selectedPixel}
+     onChange={(e) => setSelectedPixel(e.target.value)}
+     className={`py-2 px-4 rounded-full inline-flex items-center bg-white border  cursor-pointer  text-gray-700 dark:bg-gray-800 dark:text-white`}>
+     <option value=''>Select Pixel</option>
+     {pixelOptions.map((pixel) => (
+      <option key={pixel} value={pixel}>
+       {pixel}
+      </option>
+     ))}
+    </select>
+
+    {/* Event Selector */}
+    <select
+     value={selectedEvent}
+     onChange={(e) => setSelectedEvent(e.target.value)}
+     className={`py-2 px-4 rounded-full inline-flex items-center bg-white border  cursor-pointer  text-gray-700 dark:bg-gray-800 dark:text-white`}>
+     <option value=''>Select Event</option>
+     {eventOptions.map((event) => (
+      <option key={event} value={event}>
+       {event}
+      </option>
+     ))}
+    </select>
+
+    {/* Integration Type Selector */}
+    <select
+     value={selectedIntegration}
+     onChange={(e) => setSelectedIntegration(e.target.value)}
+     className={`py-2 px-4 rounded-full inline-flex items-center bg-white border  cursor-pointer  text-gray-700 dark:bg-gray-800 dark:text-white`}>
+     <option value=''>Select Integration Type</option>
+     {integrationOptions.map((integration) => (
+      <option key={integration} value={integration}>
+       {integration}
+      </option>
+     ))}
+    </select>
    </div>
 
    {results.length === 0 && (
