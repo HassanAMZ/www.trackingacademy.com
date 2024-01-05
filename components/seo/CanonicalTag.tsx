6080129452278566794217253c7c60ce7b607e96
Shahzada Ali Hassan <reactjswebdev@gmail.com>
2024-01-05 23:43:54 +0800

    fixed canonical tags

--- components/seo/CanonicalTag.tsx
+++ components/seo/CanonicalTag.tsx
@@ -1,10 +1,17 @@
 "use client";
 
-import { useRouter, usePathname } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 
-const CanonicalTag: React.FC = () => {
+const CanonicalTag = () => {
  const pathname = usePathname();
- const canonicalUrl = `${process.env.NEXT_PUBLIC_BASE_URL}${pathname}`;
+ const [canonicalUrl, setCanonicalUrl] = useState(
+  `${process.env.NEXT_PUBLIC_BASE_URL}${pathname}`
+ );
+
+ useEffect(() => {
+  setCanonicalUrl(`${process.env.NEXT_PUBLIC_BASE_URL}${pathname}`);
+ }, [pathname]);
 
  return <link rel='canonical' href={canonicalUrl} />;
 };
