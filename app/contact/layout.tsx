fa39642af5c2fb2425433123af2904612ff2f5e7
Shahzada Ali Hassan <59549481+HassanAMZ@users.noreply.github.com>
2024-12-15 13:24:36 +0500

    added testimonials to contact funnel

--- app/contact/layout.tsx
+++ app/contact/layout.tsx
@@ -1,7 +1,13 @@
 import React, { ReactNode } from "react";
 import Container from "@/components/ui/container";
 import Navbar from "@/components/global/navbar";
+import ClientTestimonial from "@/components/home/testimonaials";
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <React.Fragment>{children}</React.Fragment>;
+  return (
+    <main className="space-y-5">
+      {children}
+      <ClientTestimonial />
+    </main>
+  );
 }
--- components/home/testimonial-card.tsx
+++ components/home/testimonial-card.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React from "react";
 import { Separator } from "@/components/ui/separator";
 import Text from "@/components/ui/text";
+import { Card, CardContent } from "../ui/card";
 
 interface ClientTestimonialCardProps {
   client: {
@@ -24,31 +25,32 @@ const ClientTestimonialCard: React.FC<ClientTestimonialCardProps> = ({
   results,
 }) => {
   return (
-    <React.Fragment>
-      <div className="space-y-4 pb-2">
-        <Text as="p" variant="bodyMd">
-          <span className="text-primary">
-            {client.businessName} ({category}: {cms})
-          </span>
-          <span>: {businessDetails}</span>
-        </Text>
-        <div className="overflow-hidden rounded-md border border-secondary bg-secondary object-contain shadow filter">
-          <Image
-            src={imageUrl}
-            alt={`Client ${client.name}`}
-            width={1920}
-            height={1080}
-            className="aspect-video"
-          />
-        </div>
+    <Card className="max-w-4xl w-full mx-auto">
+      <CardContent>
+        <div className="space-y-4 py-4">
+          <Text as="p" variant="bodyMd">
+            <span className="text-primary">
+              {client.businessName} ({category}: {cms})
+            </span>
+            <span>: {businessDetails}</span>
+          </Text>
+          <div className="overflow-hidden rounded-md border border-secondary bg-secondary object-contain shadow filter">
+            <Image
+              src={imageUrl}
+              alt={`Client ${client.name}`}
+              width={1920}
+              height={1080}
+              className="aspect-video"
+            />
+          </div>
 
-        <Text as="p" variant="bodyMd">
-          <span className="text-primary">Results:&nbsp;</span>
-          <span>"{results}"</span>
-        </Text>
-      </div>
-      <Separator />
-    </React.Fragment>
+          <Text as="p" variant="bodyMd">
+            <span className="text-primary">Client's Feedback:&nbsp;</span>
+            <span>"{results}"</span>
+          </Text>
+        </div>
+      </CardContent>
+    </Card>
   );
 };
 
